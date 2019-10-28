abc1171288ddd890b7275d424f5b4601425a852d
Shifera <shiferawregassa@gmail.com>
2019-10-28 16:58:49 -0400

    first push

---
+++ src/DebuggingActivity.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class DebuggingActivity {
+    public static void main(String[] args) {
+        ArrayList al = new ArrayList();
+        al.add('C');
+        al.add('A');
+        al.add('E');
+        al.add('B');
+        al.add('D');
+        al.add('F');
+        al.add('1');
+        System.out.println(al.size());
+    }
+}
+
